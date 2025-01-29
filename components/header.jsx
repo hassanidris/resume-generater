62b70d0ae64c0ade5ce21a39238a951f22d58fd6
Hassen Ahmed <hassan.creative@gmail.com>
2025-01-29 16:07:49 +0100

    seting up inngest

--- components/header.jsx
+++ components/header.jsx
@@ -36,54 +36,68 @@ const Header = () => {
         <div className=" flex items-center space-x-2 md:space-x-4">
           <SignedIn>
             <Link href={"/dashboard"}>
-              <Button>
+              <Button variant="outline">
                 <LayoutDashboard className=" h-4 w-4" />
                 <span className=" hidden md:block">Industry Insights</span>
               </Button>
             </Link>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button>
+                  <StarsIcon className=" h-4 w-4" />
+                  <span className=" hidden md:block">Growth Tools</span>
+                  <ChevronDown className=" h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href={"/resume"} className=" flex items-center gap-2">
+                    <FileTextIcon className=" h-4 w-4" />
+                    <span>Build Resume</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link
+                    href={"/cover-letter"}
+                    className=" flex items-center gap-2"
+                  >
+                    <PenBox className=" h-4 w-4" />
+                    <span>Cover Letter</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link
+                    href={"/interview"}
+                    className=" flex items-center gap-2"
+                  >
+                    <GraduationCap className=" h-4 w-4" />
+                    <span>Interview Prep</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SignedIn>
 
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button>
-                <StarsIcon className=" h-4 w-4" />
-                <span className=" hidden md:block">Growth Tools</span>
-                <ChevronDown className=" h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Link href={"/resume"} className=" flex items-center gap-2">
-                  <FileTextIcon className=" h-4 w-4" />
-                  <span>Build Resume</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link
-                  href={"/cover-letter"}
-                  className=" flex items-center gap-2"
-                >
-                  <PenBox className=" h-4 w-4" />
-                  <span>Cover Letter</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link href={"/interview"} className=" flex items-center gap-2">
-                  <GraduationCap className=" h-4 w-4" />
-                  <span>Interview Prep</span>
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold",
+                },
+              }}
+              afterSignOutUrl="/"
+            />
+          </SignedIn>
         </div>
       </nav>
-
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </header>
   );
 };
