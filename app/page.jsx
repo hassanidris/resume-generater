611ad065869d82183e4262d019dde8277faf3ba0
Hassen Ahmed <hassan.creative@gmail.com>
2025-01-30 14:08:06 +0100

    setup status section

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { features } from "@/data/features";
+import { stats } from "@/data/stats";
 
 export default function Home() {
   return (
@@ -41,25 +42,21 @@ export default function Home() {
         </div>
       </section>
 
+      {/* <StatusSection /> */}
       <section className=" w-full py-12 md:py-24 bg-muted/50">
         <div className=" container mx-auto px-4 md:px-6">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
-            <div className="flex flex-col items-center justify-center space-y-2">
-              <h3 className=" text-4xl font-bold">50+</h3>
-              <p className=" text-muted-foreground">Industries covered</p>
-            </div>
-            <div className="flex flex-col items-center justify-center space-y-2">
-              <h3 className=" text-4xl font-bold">1000+</h3>
-              <p className=" text-muted-foreground">Interviews Questions</p>
-            </div>
-            <div className="flex flex-col items-center justify-center space-y-2">
-              <h3 className=" text-4xl font-bold">95%</h3>
-              <p className=" text-muted-foreground">Success Rate</p>
-            </div>
-            <div className="flex flex-col items-center justify-center space-y-2">
-              <h3 className=" text-4xl font-bold">24/7</h3>
-              <p className=" text-muted-foreground">AI Support</p>
-            </div>
+            {stats.map((stat) => {
+              return (
+                <div
+                  key={stat.id}
+                  className="flex flex-col items-center justify-center space-y-2"
+                >
+                  <h3 className=" text-4xl font-bold">{stat.value}</h3>
+                  <p className=" text-muted-foreground">{stat.title}</p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
---
+++ data/stats.js
@@ -0,0 +1,22 @@
+export const stats = [
+  {
+    id: 1,
+    title: "Industries covered",
+    value: "50+",
+  },
+  {
+    id: 2,
+    title: "Interviews Questions",
+    value: "1000+",
+  },
+  {
+    id: 3,
+    title: "Success Rate",
+    value: "95%",
+  },
+  {
+    id: 4,
+    title: "AI Support",
+    value: "24/7",
+  },
+];
