7827c714aed7a80b3c2d9fec6d3409edfdd39f9a
Hassen Ahmed <hassan.creative@gmail.com>
2025-03-04 22:16:27 +0100

    starting in interview mock section

---
+++ app/(main)/interview/_components/quiz.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { generateQuiz } from "@/actions/interview";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import useFetch from "@/hooks/use-fetch";
+import { useState } from "react";
+
+const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [showExplanation, setShowExplanation] = useState(false);
+
+  const {
+    loading: generatingQuiz,
+    fn: generateQuizFn,
+    data: quizData,
+  } = useFetch(generateQuiz);
+
+  if (!quizData) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Ready to test your knowledge?</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-muted-foreground">
+            This quiz contains 10 questions specific to your industry and
+            skills. Take your time and choose the best answer for each question.
+          </p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    );
+  }
+
+  return <div>Quiz</div>;
+};
+
+export default Quiz;
