c73ea1f450ca5ae6c3bc77ccd2c00f2d2f9bc975
Hassen Ahmed <hassan.creative@gmail.com>
2025-04-03 13:36:53 +0200

    hiding unnessasary sections

--- app/page.jsx
+++ app/page.jsx
@@ -55,7 +55,7 @@ export default function Home() {
       </section>
 
       {/* <StatusSection /> */}
-      <section className=" w-full py-12 md:py-24 bg-muted/50">
+      {/* <section className=" w-full py-12 md:py-24 bg-muted/50">
         <div className=" container mx-auto px-4 md:px-6">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
             {stats.map((stat) => {
@@ -71,7 +71,7 @@ export default function Home() {
             })}
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* <HowItWorksSection /> */}
       <section className=" w-full py-12 md:py-24 lg:py-32 bg-background">
